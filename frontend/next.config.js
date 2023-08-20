b2c896ecf062529ff3740bbeb9f3ec8f1a3d4e99
Kylie Crump <moarsomecoarsome@gmail.com>
2023-08-20 15:21:00 +1200

    First commit

---
+++ frontend/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /*
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+        connectionString: "mongodb+srv://vercel-admin-user:admin@cluster0.yt5iyob.mongodb.net/?retryWrites=true&w=majority",
+        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+    },
+    publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000/api' // development api
+            : 'http://localhost:3000/api' // production api
+    }
+    */
+}
+
+module.exports = nextConfig
